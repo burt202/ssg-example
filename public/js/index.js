dd67da7a48b1773ea3664fe1c383d2821a9c88b4
Aaron Burtnyk <burt202@gmail.com>
2015-09-05 10:06:17 +0100

    only show images after slick has loaded

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,10 @@
 var $ = require("jquery");
 require("slick-carousel");
 
+$(".slideshow").on("init", function () {
+  $(".slideshow img").fadeIn(500);
+});
+
 $(".slideshow").slick({
   infinite: true,
   arrows: false,
